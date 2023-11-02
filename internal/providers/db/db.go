47a50646ce3e790bfb5806e9c532699903bbb6cf
bwunna <brynovivan@yandex.ru>
2023-11-02 21:00:51 +0300

    now skills are stored in slice of strings not in table

--- internal/providers/db/db.go
+++ internal/providers/db/db.go
@@ -11,6 +11,17 @@ type DataBase struct {
 	db *sql.DB
 }
 
+func KeysInString(keys []string) string {
+	var answer string
+	for _, value := range keys {
+		answer += "'" + value + "', "
+	}
+	if len(answer) > 2 {
+		answer = answer[:len(answer)-2]
+	}
+	return answer
+}
+
 func NewDB(host, user, password, nameOfDB, driverName string, port int) (*DataBase, error) {
 	// constructor for DataBase struct
 	params := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, nameOfDB)
@@ -44,15 +55,8 @@ func (db *DataBase) AddCompanyByName(name string) error {
 
 func (db *DataBase) AddEmployee(employee models.Employee) error {
 	// adding employee and his skills
-	queryString := fmt.Sprintf("insert into employee(emp_name)\nvalues('%s')", employee.Name)
+	queryString := fmt.Sprintf("insert into employee(emp_name, skills, email) values('%s', array[%s], '%s')", employee.Name, KeysInString(employee.Skills), employee.Email)
 	_, err := db.db.Query(queryString)
-	if err != nil {
-		return err
-	}
-	for skill, _ := range employee.Skills {
-		queryString := fmt.Sprintf("INSERT INTO skill (skill, emp_id) \nSELECT '%s', emp_id FROM employee WHERE emp_name = '%s';", skill, employee.Name)
-		_, err = db.db.Query(queryString)
-	}
 	if err != nil {
 		return err
 	}
@@ -62,15 +66,8 @@ func (db *DataBase) AddEmployee(employee models.Employee) error {
 
 func (db *DataBase) AddDepartmentToCompany(companyName string, department models.Department) error {
 	// adding department to existing company
-	queryString := fmt.Sprintf("insert into department\n(dep_name, company_id)\nselect '%s', company_id from company where company_name = '%s'", department.Name, companyName)
+	queryString := fmt.Sprintf("insert into department (dep_name, company_id, required_skills) select '%s', company_id, array[%s] from company where company_name = '%s'", department.Name, KeysInString(department.RequiredSkills), companyName)
 	_, err := db.db.Query(queryString)
-	if err != nil {
-		return err
-	}
-	for skill, _ := range department.RequiredSkills {
-		queryString = fmt.Sprintf("insert into required(skill, dep_id)\nselect '%s', dep_id from department where dep_name = '%s'", skill, department.Name)
-		_, err = db.db.Query(queryString)
-	}
 	if err != nil {
 		return err
 	}
@@ -78,19 +75,21 @@ func (db *DataBase) AddDepartmentToCompany(companyName string, department models
 	return nil
 }
 
-func (db *DataBase) EmployPersonByName(name string, companyName string, departmentName string) error {
+func (db *DataBase) EmployPersonByEmail(email string, companyName string, departmentName string) error {
 	// employing person to the department of the company
+
 	queryString := fmt.Sprintf(
-		"update employee "+
-			"set dep_id = subquery.dep_id"+
-			" from (select dep_id  from department where dep_name = '%s' and company_id = (select company_id from company where company_name = '%s')) as subquery "+
-			"where emp_name = '%s' and employee.dep_id is null "+
-			"and (select count(*) from required join skill using (skill) where required.dep_id = "+
-			" (select dep_id from department where dep_name = '%s' "+
-			"  and company_id = (select company_id from company where company_name = '%s')) "+
-			" and skill.emp_id = (select emp_id from employee where emp_name = '%s')) = "+
-			" (select count(*) from required where dep_id = (select dep_id from department where dep_name = '%s' "+
-			" and company_id = (select company_id from company where company_name = '%s')));", departmentName, companyName, name, departmentName, companyName, name, departmentName, companyName)
+		"update employee set dep_id = subquery.dep_id from (select dep_id from department where dep_name = '%s'"+
+			" and company_id = (select company_id from company where"+
+			" company_name = '%s')) as subquery where email = '%s' and (select (select count(*) "+
+			"from ((select unnest(skills)"+
+			" from employee where email = '%s')"+
+			" join (select unnest(required_skills)"+
+			" from department"+
+			" where dep_name = '%s' and company_id = "+
+			" (select company_id from company where company_name = '%s')) "+
+			" using (unnest))) = (select count(*) from (select unnest(required_skills)"+
+			" from department where dep_name = '%s' and company_id = (select company_id from company where company_name = '%s'))))", departmentName, companyName, email, email, departmentName, companyName, departmentName, companyName)
 	_, err := db.db.Query(queryString)
 	if err != nil {
 		return err
