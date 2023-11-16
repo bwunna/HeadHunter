481859e4c9cab099faa141a4ba5d26d57f45b680
bwunna <brynovivan@yandex.ru>
2023-11-16 21:50:20 +0300

    service starting

--- internal/delivery/grpcServer.go
+++ internal/delivery/grpcServer.go
@@ -4,10 +4,13 @@ import (
 	"HeadHunter/internal/controller"
 	v1 "HeadHunter/internal/delivery/v1"
 	"HeadHunter/internal/providers/db"
+	"HeadHunter/internal/services/userService"
 	"HeadHunter/pkg/employmentService"
 	"fmt"
 	"google.golang.org/grpc"
+	"log"
 	"net"
+	"time"
 )
 
 // configuration for server
@@ -22,19 +25,27 @@ const (
 )
 
 func RunGRPCServer() error {
-
+	time.Sleep(time.Second * 0)
 	dataBase, err := db.NewDB(host, user, password, dbName, driverName, port)
 	if err != nil {
 		return err
 	}
-	newController, err := controller.NewController(dataBase)
-	server := v1.NewGrpcServer(newController)
-	fmt.Println("server is working")
-	lis, err := net.Listen("tcp", ":8082")
 
+	client, err := userService.Init("localhost:8080")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	newController := controller.NewController(dataBase)
+
+	server := v1.NewGrpcServer(newController, client)
+
+	fmt.Println("server is working")
+
+	lis, err := net.Listen("tcp", ":8082")
 	if err != nil {
 		return err
 	}
+
 	grpcSrv := grpc.NewServer()
 	employmentService.RegisterEmploymentCenterServer(grpcSrv, server)
 
