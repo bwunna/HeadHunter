29a34ec54583fe29c9cb4c1164bff42f203adf91
bwunna <brynovivan@yandex.ru>
2023-11-16 21:54:29 +0300

    models

---
+++ internal/delivery/v1/models.go
@@ -0,0 +1,10 @@
+package v1
+
+const (
+	MessageOK          = "OK"
+	MessageNotExecuted = "Request wasn't executed"
+	CodeOK             = 200
+	CodeNotExecuted    = 400
+	MessageNOTFOUND    = "employee wasn't found"
+	codeNOTFOUND       = 401
+)
